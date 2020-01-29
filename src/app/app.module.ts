d87f682762b6886633002d4d310da794f3ab8b75
Darwin <darwinu34@yahoo.es>
2020-01-29 18:31:28 -0500

    se inicia la creación del menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//se importa el archivo que tiene todos los modulos de material
+import { MaterialModule } from './material.module';
+//dum: componentes generados.
+import { RoutingModule } from './routing.module';
+/* import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component'; */
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    /* HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidenavListComponent */
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RoutingModule,
+    HttpClientModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
