4318589a81470aff0cf8cd4abd71f624b164953f
Darwin <darwinu34@yahoo.es>
2020-02-08 08:04:37 -0500

    se crea los botones de navegación

--- src/app/_components/compartidos/tablausuario/tablausuario.component.ts
+++ src/app/_components/compartidos/tablausuario/tablausuario.component.ts
@@ -10,7 +10,7 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class TablausuarioComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'editar', 'borrar'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -24,6 +24,18 @@ export class TablausuarioComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  editarUsuario(usurioEditar: PeriodicElement) {
+    console.log('usuario editar:', usurioEditar);
+  }
+
+  borrarUsuario(usurioBorrar: PeriodicElement) {
+    console.log('usuario borrar:', usurioBorrar);
+  }
+
+  onNuevoUsuario(){
+    console.log('nuevo usuario:');
   }
 
 }
